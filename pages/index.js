d1d418f6eedd580f55dc911458ad1383988485c9
Mihai Pocorschi <mihai@pocorschi.com>
2021-01-22 14:28:45 +0200

    added gary

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>🌮⭐️</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="🌮🌮🌮🌮🌮🌮🌮🌮🌮" />
+        <img className="gary" src="/gary.png"></img>
       </main>
-
-      <Footer />
     </div>
   )
 }
